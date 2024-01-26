2f7b49b4f2f3aadf6c498de3a0a25ddd922cf2f1
Nikki Gowan <nikkigowan2@gmail.com>
2024-01-26 12:40:27 -0500

    Added GET response for book based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,8 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;                                             //store given isbn
+  res.send(books[isbn]);                                                    //send book with isbn
  });
   
 // Get book details based on author
