e40ebe906d1c612d75b0a28d003d01d03b7525d0
Nikki Gowan <nikkigowan2@gmail.com>
2024-01-26 13:10:58 -0500

    Added GET request for book reviews based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,8 +62,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;                                             //store given isbn
+  if(books[isbn].reviews){
+    res.send(books[isbn].reviews);                                          //send book reviews for given isbn
+  }//if
+  else{
+    res.send("");                                                           //send nothing- no match
+  }//else
 });
 
 module.exports.general = public_users;
